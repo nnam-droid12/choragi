1db08d7eee2aee7b14464b84886f21091a19a590
Willams <williamekene700@gmail.com>
2026-03-07 11:32:57 +0100

    fix the error 404 model not found by switching from uscentral1 to global

---
+++ digital-promoter/src/main/java/com/choragi/promoter/config/GenAIConfig.java
@@ -0,0 +1,22 @@
+package com.choragi.promoter.config;
+
+import com.google.genai.Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GenAIConfig {
+
+    @Value("${spring.cloud.gcp.project-id}")
+    private String projectId;
+
+    @Bean
+    public Client genAiClient() {
+        return Client.builder()
+                .project(projectId)
+                .location("global")
+                .vertexAI(true)
+                .build();
+    }
+}
